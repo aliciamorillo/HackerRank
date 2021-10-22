4c9675f95cadc503010e86da129991304cf24ce9
aliciamorillo <63121998+aliciamorillo@users.noreply.github.com>
2021-10-22 10:21:51 +0200

    Add files via upload

---
+++ Java/44. Java Inheritance II.java
@@ -0,0 +1,10 @@
+
+class Arithmetic {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
+class Adder extends Arithmetic {
+
+}
---
+++ Java/47. Java Method Overriding.java
@@ -0,0 +1,7 @@
+
+
+    @Override
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has 11 players in " + getName());
+    }
+
