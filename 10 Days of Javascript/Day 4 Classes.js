f41238b5ace4dfaecf31fa214623af7b889f2cbc
aliciamorillo <63121998+aliciamorillo@users.noreply.github.com>
2021-10-26 15:38:12 +0200

    Add files via upload

---
+++ 10 Days of Javascript/Day 4 Classes.js
@@ -0,0 +1,20 @@
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+
+class Polygon {
+    constructor(heights){
+        var polygonPerimeter = 0;
+        for(var index in heights){
+            polygonPerimeter += heights[index];
+        }
+        
+        this.myPerimeter = polygonPerimeter;
+    }
+    
+    perimeter(){
+        return this.myPerimeter;
+    }
+}
