ffcbf9c841ed0fd47c6c829ae2e01834d5f230d1
aliciamorillo <63121998+aliciamorillo@users.noreply.github.com>
2021-10-28 08:24:23 +0200

    Add files via upload

---
+++ 10 Days of Javascript/Day 7 Regular Expressions II.js
@@ -0,0 +1,15 @@
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+     * followed by one or more letters.
+     */
+    
+    var re = RegExp('\\d+', 'g');
+    
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
