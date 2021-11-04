67431da48bf551f573d73e1717367189cee35ed2
aliciamorillo <63121998+aliciamorillo@users.noreply.github.com>
2021-11-04 09:48:08 +0100

    Update Arithmetic Operations.sh

--- Linux Shell/Bash/Arithmetic Operations.sh
+++ Linux Shell/Bash/Arithmetic Operations.sh
@@ -1,3 +1,6 @@
+# A mathematical expression containing +,-,*,^, / and parenthesis will be provided. Read in the expression, then evaluate it.
+# Display the result rounded to 3 decimal places.
+
 read x
 
 printf "%.3f" `echo "$x" | bc -l`
