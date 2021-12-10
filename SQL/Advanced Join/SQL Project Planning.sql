56f8ba0945642c5cc27ddbdaabda6c7424601c3a
aliciamorillo <63121998+aliciamorillo@users.noreply.github.com>
2021-12-10 08:03:40 +0100

    Add files via upload

---
+++ SQL/Advanced Join/SQL Project Planning.sql
@@ -0,0 +1,9 @@
+SELECT start_date, MIN(end_date)
+FROM(SELECT start_date FROM projects
+      WHERE start_date NOT IN (SELECT end_date FROM projects)) a
+      INNER JOIN
+     (SELECT end_date FROM projects
+      WHERE end_date NOT IN (SELECT start_date FROM projects)) b
+WHERE start_date < end_date
+GROUP BY start_date
+ORDER BY MIN(end_date) - start_date, start_date;
